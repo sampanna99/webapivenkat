298dc6801a85f98a7d16fd2a19125e32a7abefd4
sampanna pokhrel <sampanna99@gmail.com>
2016-11-14 23:36:15 -0600

    web api venkat

--- DemoWebApi/App_Start/WebApiConfig.cs
+++ DemoWebApi/App_Start/WebApiConfig.cs
@@ -25,6 +25,9 @@ namespace DemoWebApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
---
+++ DemoWebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeDataAccess;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace DemoWebApi.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
